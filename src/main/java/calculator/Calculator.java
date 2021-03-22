780109de95768e16727cc92547f445b74d53a307
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-03-22 11:12:06 +0100

    Uppgift 1 Step 1 - Created simple calculator with three tests

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class Calculator {
+
+    public int add(String numbers) {
+        String[] arrayNumbers;
+        int addingNumbers = 0;
+
+        if (numbers.length() == 0) {
+            return 0;
+        }
+
+        arrayNumbers = numbers.split(",");
+
+        for (String arrayNumber : arrayNumbers) {
+            addingNumbers += Integer.parseInt(arrayNumber);
+        }
+
+        return addingNumbers;
+
+    }
+
+}
