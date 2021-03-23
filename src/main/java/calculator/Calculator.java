f3c904a95068e3f0eebd47e619a5526109abc974
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-03-23 13:48:22 +0100

    Uppgift 1 Changed add method to split everything except 0-9 and -

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,17 +1,16 @@
 package calculator;
 
+
 public class Calculator {
 
     public int add(String numbers) {
-        String[] arrayNumbers;
         int addingNumbers = 0;
         boolean illegalException = false;
         StringBuilder negativeNumbers = new StringBuilder();
 
-        arrayNumbers = numbers.split("[\\[\\]\n,;/*%]");
+        String[] arrayNumbers = numbers.split("[^0-9-]");
 
         for (String number : arrayNumbers) {
-
             if (!number.isEmpty() && Integer.parseInt(number) <= 1000) {
                 addingNumbers += Integer.parseInt(number);
 
